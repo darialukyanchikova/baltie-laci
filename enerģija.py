ac391920b6412240f03ebf79559fcc2036d72527
Darja <79309616+darialukyanchikova@users.noreply.github.com>
2021-04-16 21:51:23 +0300

    Add files via upload

---
+++ enerģija.py
@@ -0,0 +1,6 @@
+def ledaja_energija(m,l):
+    return (l*m)
+m = int(input("Ievadi ledaja masu, kg - ")) 
+l = print(3,4*(10**5))
+Q = ledaja_energija(m,l) 
+print("Energija, kura radas, kad izkust ledajs - ",Q, "Dz") 
