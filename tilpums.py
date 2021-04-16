14435fab738c6910f46f886503ba473f29100d44
Darja <79309616+darialukyanchikova@users.noreply.github.com>
2021-04-16 21:54:05 +0300

    Add files via upload

---
+++ tilpums.py
@@ -0,0 +1,6 @@
+def udens_tilpums(m,b):
+    return (b*m)
+m = int(input("Ievadi ledaja masu, kg - ")) 
+b = print("923")
+V = udens_tilpums(1000,923) 
+print("Udens tilpums, kas rodas, kad izkust ledajs - ",V, "m**3")  
