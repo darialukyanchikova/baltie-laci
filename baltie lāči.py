65101c4d5c919589134ca1f26934b010e1e11742
Darja <79309616+darialukyanchikova@users.noreply.github.com>
2021-04-16 21:50:29 +0300

    Add files via upload

---
+++ baltie lāči.py
@@ -0,0 +1,6 @@
+from math import*
+print("Ir zinams, ka pavisam uz Zemes dzivo aptuveni 25000 balto lacu. Pec interneta iegutajiem datiem mes zinam ari to, ka pec 30 gadiem nomirs 30% no pasreizeja balto lacu skaita. Mums vajag uzzinat 3 datus: cik balto lacu nomirs pec 30 gadiem, cik balto lacu nomir gada un cik gadi var paiet, kad nomirs visi balto lacu populacijas parstavji")
+a = print((25000/100)*30) #aprekinam, cik balto lacu nomirs pec 30 gadiem
+b = print(a/30) #aprekinam, cik balto lacu nomir gada
+c = print(25000/b) #aprekinam, pec cik gadiem izmirs visi laci
+print("IETEIKUMS: lai saglabatu balto lacu populaciju uz Zemes, mums vajag rupeties par musu planetu, jo globalas sasilsanas del kust ledaji un iet boja baltie laci (un ne tikai vini)")
